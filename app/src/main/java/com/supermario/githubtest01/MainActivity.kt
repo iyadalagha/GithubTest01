20414165882ed860bbf0d30c1d89d995131ddd01
pavlion <iyadalagha"hotmail.com>
2021-11-28 21:53:04 +0200

    ttt

--- app/src/main/java/com/supermario/githubtest01/MainActivity.kt
+++ app/src/main/java/com/supermario/githubtest01/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("ttt","Hello")
+        Log.d("ttt","Hello World")
+        Log.d("ggg","ggg")
     }
 }
